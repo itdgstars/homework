fe18819cc53403c3370837b176367af3f00a13c0
SergeyK <it-dgstars@mail.ru>
2023-02-01 16:10:12 +0300

    Решили третью задачу

---
+++ hw3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num%2) > 0)
+{
+    Console.WriteLine("Ваше число НЕчетное");
+}
+else 
+{
+     Console.WriteLine("Ваше число четное");
+}
