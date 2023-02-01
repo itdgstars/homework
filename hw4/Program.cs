95f137acb1771e430a7174f50cdf9a184ba319d9
SergeyK <it-dgstars@mail.ru>
2023-02-01 16:23:27 +0300

    Решили четвертую задачу

---
+++ hw4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int current_num = 1;
+
+while (current_num < N)
+{
+if ((current_num%2) == 0)
+{
+  Console.WriteLine(current_num);
+  current_num++ ;
+}
+else
+{
+    current_num++ ;
+}
+};
