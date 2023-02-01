9a3bc4cbf20b9049fb8003c380627480f0af62ef
SergeyK <it-dgstars@mail.ru>
2023-02-01 16:04:11 +0300

    Решили вторую задачу

---
+++ hw2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max)
+{
+    max = num2;
+}
+else {
+       max = num1;
+    }
+if (num3 > max)
+{
+    num3 = max;
+}
+else
+{
+    max = max;
+}
+Console.WriteLine ("Самое большое число = " + max);
